a0ec44f6eb0cab07506e6aa619133d5e468cbd42
royernst <roy.e31@hotmail.com>
2017-10-13 11:31:27 -0400

    Initial commit.  Established classes and account class fields

---
+++ BankAccount/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    abstract class Account
+    {
+        private string accountNumber;
+        private int accountBalance;
+        private string accountType;
+        
+        public string AccountNumber
+        {
+            get { return this.accountNumber; }
+            set { this.accountNumber = value; }
+        }
+
+        public int AccountBalance
+        {
+            get { return this.accountBalance; }
+            set { this.accountBalance = value; }
+        }
+
+        public string AccountType
+        {
+            get { return this.accountType; }
+            set { this.accountType = value; }
+        }
+
+    }
+}
